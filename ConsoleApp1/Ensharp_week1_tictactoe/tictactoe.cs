141209e09d0178b3e1717f073a4351e41f9a63d3
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-04-07 19:36:56 +0900

    no message

--- ConsoleApp1/Ensharp_week1_tictactoe/tictactoe.cs
+++ ConsoleApp1/Ensharp_week1_tictactoe/tictactoe.cs
@@ -86,10 +86,19 @@ namespace tictactoe
         private string a2 = "   ", b2 = "   ", c2 = "   ", d2 = "   ", e2 = "   ", f2 = "   ", g2 = "   ", h2 = "   ", i2 = "   ";
         private int cnt=1;
         private int win1=0, win2=0, comwin=0, userwin=0;
+        Random random = new Random();
 
-        public void Display1() //버전1
+        /// /////////////////////////////////
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //1~9 들어가는 리스트 
+        List<int> selectedNumbers = new List<int>();
+        //유저가 고른 + 컴퓨터가 고른 숫자 저장
+
+        public void Display1() ///////////////////////////버전1
         {
             Console.Clear();
+            cnt++;
+            Judge1();
             Console.WriteLine("        <Scoreboard>\n");
             Console.WriteLine(" ★*user*★  ★*Computer*★   ");
             Console.WriteLine("     " + userwin + "             " + comwin);
@@ -114,30 +123,184 @@ namespace tictactoe
             Console.WriteLine("l" + g1 + "I" + h1 + " I" + i1 + " I");
             Console.WriteLine("----------------");
 
-            if (cnt % 2 != 0)
+            
+
+            if (cnt % 2 == 1)
             {
-                user1();
+                ver1user();
+            }
+
+
+            else if (cnt % 2 == 0)
+            {
+                ver1com();
 
             }
-            else
-            {
-
-            }
-            Random rand = new Random();
-
-            
-             Console.WriteLine(rand.Next(1,10)); //1이상 10미만의 랜덤 숫자 생성
-
-            
-            
+           
 
         }
-        public void Display2() { //버전2
+
+        public void ver1user()
+        {
+            int essence;
+            essence = int.Parse(ReadLine());
+            selectedNumbers.Add(essence);
+            numbers.Remove(essence);
+            switch (essence)
+            {
+                case 0:
+                    Menu menu = new Menu();
+                    menu.Display();
+                    break;
+
+                case 1:
+                    a1 = " O ";
+                    break;
+
+                case 2:
+                    b1 = " O ";
+                    break;
+                case 3:
+                    c1 = " O ";
+                    break;
+                case 4:
+                    d1 = " O ";
+                    break;
+                case 5:
+                    e1 = " O ";
+                    break;
+                case 6:
+                    f1 = " O ";
+                    break;
+                case 7:
+                    g1 = " O ";
+                    break;
+                case 8:
+                    h1 = " O ";
+                    break;
+                case 9:
+                    i1 = " O ";
+                    break;
+
+            }
+
+            //유저의 입력값에 따라서 a~i 변수에 'O' 를 넣어주고, 바로 틱택토 화면에 적용되도록 display 메소드 호출
+
+            Display1();
+        }
+
+        public void ver1com()
+        {//컴퓨터
+            int randomNumber = random.Next(1, 10); // 1부터 9까지의 숫자 중 랜덤 선택
+
+            if (!selectedNumbers.Contains(randomNumber)) // 선택한 수가 이전에 선택된 적이 없으면
+            {
+                selectedNumbers.Add(randomNumber); // 선택한 수를 리스트에 추가
+                numbers.Remove(randomNumber); // 선택한 수를 리스트에서 제거
+                
+            }
+            else // 선택한 수가 이전에 선택된 적이 있으면
+            {
+                randomNumber = random.Next(1, 10); // 다시 선택
+            }
+
+            switch (randomNumber)
+            {
+                case 1:
+                    a1 = " X ";
+                    break;
+
+                case 2:
+                    b1 = " X ";
+                    break;
+                case 3:
+                    c1 = " X ";
+                    break;
+                case 4:
+                    d1 = " X ";
+                    break;
+                case 5:
+                    e1 = " X ";
+                    break;
+                case 6:
+                    f1 = " X ";
+                    break;
+                case 7:
+                    g1 = " X ";
+                    break;
+                case 8:
+                    h1 = " X ";
+                    break;
+                case 9:
+                    i1 = " X ";
+                    break;
+            }
+
+            Display1();
+
+        }
+
+        public void Judge1() //틱택토 화면에서 빙고가 나왔는지 판단하는 메소드
+                             // 빙고가 나왔다면 resetting 메소드로 a~i 변수의 값을 공백으로 초기화하고, 이긴 유저의 점수 하나 올리기
+        {
+            //가로빙고
+
+            if (a1.Trim() == "O" && b1.Trim() == "O" && c1.Trim() == "O" || d1.Trim() == "O" && e1.Trim() == "O" && f1.Trim() == "O" || g1.Trim() == "O" && h1.Trim() == "O" && i1.Trim() == "O")
+            {  //Trim -> 양쪽 공백을 제거                                                                                                          
+                userwin++;
+
+                resetting1();
+
+            }
+            if (a1.Trim() == "X" && b1.Trim() == "X" && c1.Trim() == "X" || d1.Trim() == "X" && e1.Trim() == "X" && f1.Trim() == "X" || g1.Trim() == "X" && h1.Trim() == "X" && i1.Trim() == "X")
+            {
+                comwin++;
+                resetting1();
+
+            }
+            //세로빙고
+            if (a1.Trim() == "O" && d1.Trim() == "O" && g1.Trim() == "O" || b1.Trim() == "O" && e1.Trim() == "O" && h1.Trim() == "O" || c1.Trim() == "O" && f1.Trim() == "O" && i1.Trim() == "O")
+            {
+                userwin++;
+                resetting1();
+
+            }
+
+            if (a1.Trim() == "X" && d1.Trim() == "X" && g1.Trim() == "X" || b1.Trim() == "X" && e1.Trim() == "X" && h1.Trim() == "X" || c1.Trim() == "X" && f1.Trim() == "X" && i1.Trim() == "X")
+            {
+                comwin++;
+                resetting1();
+
+            }
+
+            //대각선빙고
+
+            if (a1.Trim() == "O" && e1.Trim() == "O" && i1.Trim() == "O" || c1.Trim() == "O" && e1.Trim() == "O" && g1.Trim() == "O")
+            {
+                userwin++;
+                resetting1();
+
+            }
+
+            if (a1.Trim() == "X" && e1.Trim() == "X" && i1.Trim() == "X" || c1.Trim() == "X" && e1.Trim() == "X" && g1.Trim() == "X")
+            {
+                comwin++;
+                resetting1();
+
+            }
+
+
+        }
+
+
+        /// //////////////////////////////////
+
+        public void Display2() { ///////////////////////////////////버전2
             
             Console.Clear();
 
             cnt++;
-            Judge();
+            Judge2();
             Console.WriteLine("        <Scoreboard>\n");
             Console.WriteLine(" ★*user1*★  ★*user2*★   ");
             Console.WriteLine("     " + win1 + "           " + win2);
@@ -271,8 +434,8 @@ namespace tictactoe
 
             Display2();
         }
-
-        public void Judge() //틱택토 화면에서 빙고가 나왔는지 판단하는 메소드
+        
+        public void Judge2() //틱택토 화면에서 빙고가 나왔는지 판단하는 메소드
                             // 빙고가 나왔다면 resetting 메소드로 a~i 변수의 값을 공백으로 초기화하고, 이긴 유저의 점수 하나 올리기
         {
             //가로빙고
@@ -281,27 +444,27 @@ namespace tictactoe
             {  //Trim -> 양쪽 공백을 제거                                                                                                          
                 win1++;
 
-                resetting();
+                resetting2();
 
             }
             if (a2.Trim() == "X" && b2.Trim() == "X" && c2.Trim() == "X" || d2.Trim() == "X" && e2.Trim() == "X" && f2.Trim() == "X" || g2.Trim() == "X" && h2.Trim() == "X" && i2.Trim() == "X")
             {
                 win2++;
-                resetting();
+                resetting2();
 
             }
             //세로빙고
              if (a2.Trim() == "O" && d2.Trim() == "O" && g2.Trim() == "O" || b2.Trim() == "O" && e2.Trim() == "O" && h2.Trim() == "O" || c2.Trim() == "O" && f2.Trim() == "O" && i2.Trim() == "O")
                 {
                     win1++;
-                resetting();
+                resetting2();
 
                 }
 
                 if (a2.Trim() == "X" && d2.Trim() == "X" && g2.Trim() == "X" || b2.Trim() == "X" && e2.Trim() == "X" && h2.Trim() == "X" || c2.Trim() == "X" && f2.Trim() == "X" && i2.Trim() == "X")
                 {
                     win2++;
-                resetting();
+                resetting2();
 
                 }
 
@@ -310,14 +473,14 @@ namespace tictactoe
                 if (a2.Trim() == "O" && e2.Trim() == "O" && i2.Trim() == "O" || c2.Trim() == "O" && e2.Trim() == "O" && g2.Trim() == "O")
                 {
                     win1++;
-                resetting();
+                resetting2();
 
                 }
 
                 if (a2.Trim() == "X" && e2.Trim() == "X" && i2.Trim() == "X" || c2.Trim() == "X" && e2.Trim() == "X" && g2.Trim() == "X")
                 {
                     win2++;
-                resetting();
+                resetting2();
 
                 }
 
@@ -352,15 +515,28 @@ namespace tictactoe
                 }
             }
 
-            
-
-            
 
 
 
-        
 
-        public void resetting() //변수 a~i의 값을 초기화하는 메소드
+
+        public void resetting1() //변수 a~i의 값을 초기화하는 메소드
+        {
+            a1 = "   ";
+            b1 = "   ";
+            c1 = "   ";
+            d1 = "   ";
+            e1 = "   ";
+            f1 = "   ";
+            g1 = "   ";
+            h1 = "   ";
+            i1 = "   ";
+            cnt = 0;
+
+        }
+
+
+        public void resetting2() //변수 a~i의 값을 초기화하는 메소드
         {
             a2 = "   ";
             b2 = "   ";
