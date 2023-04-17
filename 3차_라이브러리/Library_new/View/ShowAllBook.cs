e235edd19693c8fbdb8525e6ef9f81402db75e8b
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-04-17 23:39:43 +0900

    [REFACTORING] VIEW

---
+++ 3차_라이브러리/Library_new/View/ShowAllBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using System.Threading.Tasks;
+using Library;
+using System.Text.RegularExpressions;
+using Library.Model;
+using Library.View;
+using Library.Controller;
+using static Library.Controller.LoginOrNewmember;
+
+namespace Library.View
+{
+    internal class ShowAllBook
+    {
+        private BookData bookData;
+        private UserData userData;
+
+        public ShowAllBook(BookData bookData, UserData userData)
+        {
+            this.bookData = bookData;
+            this.userData = userData;
+        }
+        public void allBook()
+        {
+
+            foreach (BookConstructor book in bookData.BookList)  //전체 도서 표시
+            {
+                Console.WriteLine("=================================================");
+                Console.WriteLine("  ID: " + book.id);
+                Console.WriteLine("  Title: " + book.bookName);
+                Console.WriteLine("  Author: " + book.author);
+                Console.WriteLine("  Publisher: " + book.publisher);
+                Console.WriteLine("  price: " + book.price);
+                Console.WriteLine("  quantity: " + book.quantity);
+                Console.WriteLine("  publicationDate: " + book.publicationDate);
+                Console.WriteLine("  isbn: " + book.isbn);
+                Console.WriteLine("  info: " + book.info);
+                Console.WriteLine("  대여 가능한 책: " + book.rentpossible + "권");
+                Console.WriteLine("==================================================");
+            }
+        }
+    }
+}
