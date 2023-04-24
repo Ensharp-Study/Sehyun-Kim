818d06d7939ac13f54846bb51b8dedca83fa507f
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-04-24 12:26:56 +0900

    [PROGRAM]

---
+++ 4차_LTT(구)/LTT/Controller/LengthOrganize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LTT.Controller
+{
+    internal class LengthOrganize
+    {
+       
+    }
+}
---
+++ 4차_LTT(구)/LTT/Controller/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using System.Threading.Tasks;
+using LTT.View;
+using LTT.Model;
+
+
+namespace LTT.Controller
+{
+    public class LTT
+    {
+        public static void Main(String[] args)
+        {
+            DisplayLogin displayLogin = new DisplayLogin();
+            MenuDisplay menuDisplay = new MenuDisplay();
+            SearchTimeTable searchTimeTable = new SearchTimeTable();
+            //DataInExcel dataInExcel = new DataInExcel();
+            //dataInExcel.OpenExcel();
+            searchTimeTable.SearchingTimeTable();
+            //menuDisplay.DisplayMenu();
+            //displayLogin.InitialDisplay();
+
+        }
+    }
+}
