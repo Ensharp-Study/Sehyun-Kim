1e0878c2d6cc73f278cfaef6976e51a73380ed60
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-04-24 12:21:51 +0900

    [ADD] 로그인 메소드

---
+++ 4차_LTT(구)/LTT/Controller/LoginProcess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LTT.View;
+
+namespace LTT.Controller
+{
+    internal class LoginProcess
+    {
+        public void ProcessOfLogin() //로그인
+        {
+            /*
+            GetHiddenConsoleInput getHiddenConsoleInput = new GetHiddenConsoleInput();
+            string inputId = Console.ReadLine();
+
+            
+            string inputPw = getHiddenConsoleInput.HideConsoleInput();
+
+            bool isLogin = false;
+            
+            int i = 0;
+            foreach (var user in this.userData.UserList)
+            {
+                i++;
+                if (user.UserId == inputId && user.password == inputPw)
+                {
+                    Console.Clear();
+                    Console.WriteLine("로그인 성공!");
+
+                    isLogin = true;
+                    userlistnumber = i - 1;
+                    i = 0;
+
+                    viewMenu.ViewUserMenu();
+                    int num = int.Parse(Console.ReadLine());
+                    menuConstant.ConstantOfUserMenu(num);
+                    break;
+                }
+            }
+
+            if (!isLogin)
+            {
+                Console.Clear();
+                Console.WriteLine("로그인 실패. 아이디 또는 비밀번호가 일치하지 않습니다.");
+               
+
+            }
+
+            */
+
+
+
+        }
+    }
+}
