8dafd7af28a9e477259e7bf908cf57badddaefda
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-04-28 13:02:09 +0900

    [ADD] 수강신청 메뉴 추가

---
+++ 4차_LTT(구)/LTT/View/MenuOfRegisterLecture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTT.View
+{
+    internal class MenuOfRegisterLecture
+    {
+        public void ViewMenuOfRegisterLecture()
+        {
+            Console.Clear();
+            Console.WriteLine("            ┌-:*=.-.*:*-=*-.-:*-.-:*=.-.*:*-=*-.-:*=.-.*:*-=*-.-:*=.-**-.-:*=.-.-=*-.-:*:┐   ");
+            Console.WriteLine("");
+            Console.WriteLine("                                             < 수강신청 >");
+            Console.WriteLine("");
+            Console.WriteLine("                                           1.  강의 검색                            ");
+            Console.WriteLine("                                                   ");
+            Console.WriteLine("                                           2.  수강신청 강의 추가                                 ");
+            Console.WriteLine("                                                                              ");
+            Console.WriteLine("                                           3. 수강신청 강의 삭제                     ");
+            Console.WriteLine("                                               ");
+            Console.WriteLine("                                           4. 수강신청 강의 조회                      ");
+            Console.WriteLine("                                          ");
+            Console.WriteLine("                                           5. 시간표  ");
+            Console.WriteLine("            ┗*-.-:*=.-.*:*-=*-.-:*-.-:*=.-.*:*-=*-.-:*=.-.*:*-=*-.-:*=.-**-.-:*=.-.-=*-.:┛    \n     ");
+
+            int inputnumber = 0;
+            inputnumber = int.Parse(Console.ReadLine());
+
+            switch (inputnumber)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+
+            }
+        }
+
+        
+    }
+}
