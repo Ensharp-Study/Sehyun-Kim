4cda35dd988b6d96ae544d629c0fb5c45f665df3
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-05-05 21:21:21 +0900

    [ADD] DUMP 폴더 추가

---
+++ 3차_라이브러리/etc/김세현/Dump20230505/member1_returnlist.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE  IF NOT EXISTS `member1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `member1`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: member1
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `returnlist`
+--
+
+DROP TABLE IF EXISTS `returnlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `returnlist` (
+  `id` int DEFAULT NULL,
+  `bookName` varchar(45) DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  `publisher` varchar(45) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `publicationDate` varchar(45) DEFAULT NULL,
+  `isbn` varchar(45) DEFAULT NULL,
+  `info` varchar(45) DEFAULT NULL,
+  `rentpossible` int DEFAULT NULL,
+  `borrowtime` varchar(45) DEFAULT NULL,
+  `returntime` varchar(45) DEFAULT NULL,
+  `userid` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `returnlist`
+--
+
+LOCK TABLES `returnlist` WRITE;
+/*!40000 ALTER TABLE `returnlist` DISABLE KEYS */;
+INSERT INTO `returnlist` VALUES (203,'아','어','리',2,0,'2003-04-50','3432','영어',2,'2023-05-05 20:29:36','2023-05-05 20:57:07','SEHYUN'),(203,'아','어','리',2,0,'2003-04-50','3432','영어',1,'2023-05-05 20:30:31','2023-05-05 20:57:07','SEHYUN'),(394,'아이스티의맛','맛있음','보성홍차',2,0,'2023-05-05','203423492','제로칼로리',2,'2023-05-05 20:35:16','2023-05-05 20:58:28','SEHYUN');
+/*!40000 ALTER TABLE `returnlist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-05 21:19:06
