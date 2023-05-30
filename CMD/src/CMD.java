254c966735ae45e8c159dab63d3cfc17e2cc6210
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-05-31 02:28:04 +0900

    [modify] 입력 로직 수정

--- CMD/src/CMD.java
+++ CMD/src/CMD.java
@@ -26,15 +26,7 @@ public class CMD {
 
     private void executeCommand(String command) {
         if (command.equals("hello")) {
-            System.out.println("Hello, world!");
-        } else if (command.equals("date")) {
-            // 현재 날짜 및 시간 출력
-            // 예시: System.out.println(new java.util.Date());
-        } else if (command.equals("help")) {
-            // 도움말 출력
-            System.out.println("Available commands: hello, date, help, exit");
-        } else if (!command.equals("exit")) {
-            System.out.println("Unknown command: " + command);
+            System.out.println(" ");
         }
     }
 }
