63557c0fe452f113e7c06f734419d356b2f50a99
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-06-01 01:38:03 +0900

    [ADD] Mode 클래스 추가 [ADD] enterMode 메소드 추가

--- CMD/src/CMD.java
+++ CMD/src/CMD.java
@@ -3,30 +3,54 @@ import java.util.Scanner;
 public class CMD {
     public static void main(String[] args) {
         CMD cmd = new CMD();
-        cmd.start();
+        cmd.run();
     }
 
-    public void printMessage(){
+    private void printMessage(){
         System.out.println("Microsoft Windows [Version 10.0.22621.1555]");
         System.out.println("(c) Microsoft Corporation. All rights reserved.");
     }
 
-    public void start() {
+    private void run() {
         Scanner scanner = new Scanner(System.in);
         String command;
         printMessage();
         do {
             System.out.print("C:\\Users\\user>>");
             command = scanner.nextLine();
-            executeCommand(command);
+            enterMode(command);
         } while (!command.equals("exit"));
 
         scanner.close();
     }
 
-    private void executeCommand(String command) {
-        if (command.equals("hello")) {
-            System.out.println(" ");
+    private void enterMode(String command) {
+        Mode mode = new Mode();
+        String[] token = command.split("\\s"); //command를 공백 기준으로 자르기
+        String[] arguments = new String[token.length - 1]; //
+        System.arraycopy(token, 1, arguments, 0, arguments.length);
+
+        if (token[0].equals("cd")){
+            mode.enterCdMode(arguments);
+        }
+        else if (token[0].equals("dir")){
+
+        }
+        else if (token[0].equals("cls")){
+
+        }
+        else if (token[0].equals("help")){
+
+        }
+        else if (token[0].equals("copy")){
+
+        }
+        else if (token[0].equals("move")){
+
+        }
+        else {
+            System.out.print("'"+command+"'"+"은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는\n");
+            System.out.print("배치 파일이 아닙니다.\n\n");
         }
     }
 }
---
+++ CMD/src/Mode.java
@@ -0,0 +1,5 @@
+public class Mode {
+    public void enterCdMode(String[] arguments){
+
+    }
+}
