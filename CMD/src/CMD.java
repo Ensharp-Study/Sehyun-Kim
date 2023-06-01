1c5d61c3f134f7e22279b0ea2409924b6278f160
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-06-02 01:35:03 +0900

    [modify] tostartwith 사용해서 명령어 판단하도록 수정 [modify] cd에서, cd..과 cd .. 둘 다 작동하도록 수정

--- CMD/src/CMD.java
+++ CMD/src/CMD.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class CMD {
-    public static void main(String[] args) {
-        CMD cmd = new CMD();
-        cmd.run();
-    }
+    private String currentDrive; //현재 드라이브의 정보를 담을 변수
+    private String currentDirectory; //현재 디렉터리의 정보를 담을 변수
 
     private void printMessage(){
         System.out.println("Microsoft Windows [Version 10.0.22621.1555]");
         System.out.println("(c) Microsoft Corporation. All rights reserved.");
+        currentDrive="C:\\";
+        currentDirectory="Users\\user";
     }
 
-    private void run() {
+    //실행 메소드
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         String command;
         printMessage();
+        //exit이 입력되기 전까지 반복
         do {
-            System.out.print("C:\\Users\\user>>");
+            System.out.print(currentDrive + currentDirectory+">");
             command = scanner.nextLine();
             enterMode(command);
         } while (!command.equals("exit"));
@@ -24,32 +26,29 @@ public class CMD {
         scanner.close();
     }
 
+    //모드로 진입하는 메소드
     private void enterMode(String command) {
         Mode mode = new Mode();
-        String[] token = command.split("\\s"); //command를 공백 기준으로 자르기
-        String[] arguments = new String[token.length - 1]; //
-        System.arraycopy(token, 1, arguments, 0, arguments.length);
-
-        if (token[0].equals("cd")){
-            mode.enterCdMode(arguments);
+        if (command.startsWith("cd")) {
+            mode.enterCdMode(command, currentDrive, currentDirectory);
         }
-        else if (token[0].equals("dir")){
-
+        else if (command.equals("dir")) {
+            // Handle dir command
         }
-        else if (token[0].equals("cls")){
-
+        else if (command.equals("cls")) {
+            // Handle cls command
         }
-        else if (token[0].equals("help")){
-
+        else if (command.equals("help")) {
+            // Handle help command
         }
-        else if (token[0].equals("copy")){
-
+        else if (command.equals("copy")) {
+            // Handle copy command
         }
-        else if (token[0].equals("move")){
-
+        else if (command.equals("move")) {
+            // Handle move command
         }
         else {
-            System.out.print("'"+command+"'"+"은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는\n");
+            System.out.print("'" + command + "'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는\n");
             System.out.print("배치 파일이 아닙니다.\n\n");
         }
     }
---
+++ CMD/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CMD cmd = new CMD();
+        cmd.run();
+    }
+}
