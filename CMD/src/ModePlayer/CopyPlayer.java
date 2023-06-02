f117b4ccfc5454a8dd345c13ed5141f431a710d6
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-06-02 18:19:12 +0900

    [modify] copyplayer 기능 작동하도록 수정중

--- CMD/src/ModePlayer/CopyPlayer.java
+++ CMD/src/ModePlayer/CopyPlayer.java
@@ -11,25 +11,29 @@ public class CopyPlayer {
         this.currentDrive = currentDrive;
         this.currentDirectory = currentDirectory;
     }
-    public void enterCopyMode(String command, String currentDrive, String currentDirectory){
-        String commandTokens = command.substring(5); //copy+" " 제외 나머지 문자열
-        int spaceIndex = commandTokens.indexOf(" "); //공백이 몇 번째 인덱스에서 나오는지 찾기
-        String sourcePath = commandTokens.substring(0, spaceIndex-1 ); //공백 이전 문자열 저장
-        String destinationPath = commandTokens.substring(spaceIndex); //공백 이후 문자열 저장
+    public void enterCopyMode(String command, String currentDrive, String currentDirectory) {
+        String commandTokens = command.substring(5); // copy+" " 제외 나머지 문자열
+        int spaceIndex = commandTokens.indexOf(" "); // 공백이 몇 번째 인덱스에서 나오는지 찾기
+        String sourcePath = commandTokens.substring(0, spaceIndex); // 공백 이전 문자열 저장
+        String destinationPath = commandTokens.substring(spaceIndex + 1); // 공백 이후 문자열 저장
 
-        File sourceFile = new File(currentDirectory, sourcePath); //sourcePath에 해당하는 파일 찾기
-        File destinationDir = new File(currentDirectory, destinationPath); //destinationPath에 해당하는 파일 찾기
+        File sourceFile = new File(currentDirectory, sourcePath); // sourcePath에 해당하는 파일 찾기
+        File destinationDir = new File(currentDirectory, destinationPath); // destinationPath에 해당하는 폴더 찾기
 
-        if (sourceFile.exists() && destinationDir.exists()&&destinationDir.isDirectory()){
-            //파일이 존재하는지, destinationDir이 디렉터리 경로인지 확인
+        if (sourceFile.exists() && destinationDir.exists() && destinationDir.isDirectory()) {
+            // 파일이 존재하는지, destinationDir이 디렉터리 경로인지 확인
             Path source = sourceFile.toPath();
             Path destination = new File(destinationDir, sourceFile.getName()).toPath();
-            Files.copy(source, destination, StandardCopyOption.COPY_ATTRIBUTES);
-            System.out.println("!");
-        }
-        else{
-            System.out.println("지정된 파일을 찾을 수 없습니다.");
-        }
 
+            // Files.copy는 IOException을 발생시키므로 try-with-resources 사용하여 예외 처리
+            try {
+                Files.copy(source, destination, StandardCopyOption.COPY_ATTRIBUTES);
+                System.out.println("1개 파일이 복사되었습니다.");
+            } catch (Exception e) {
+                System.out.println("지정된 경로를 찾을 수 없습니다.");
+            }
+        } else {
+            System.out.println("지정된 경로를 찾을 수 없습니다.");
+        }
     }
 }
