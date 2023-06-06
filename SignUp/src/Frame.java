4c8e9976101c18086d85cdc0fdb96c0efb1d962b
sehxxnee <sy2yrx4w4y@privaterelay.appleid.com>
2023-06-07 02:51:28 +0900

    [add] 프레임 생성

---
+++ SignUp/src/Frame.java
@@ -0,0 +1,31 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+    public void createFrame() {
+        Image background;
+
+        setTitle("마인크래프트");
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        background = new ImageIcon("C:\\Users\\user\\OneDrive\\바탕 화면\\엔샵크래프트.png").getImage();
+        JPanel panel = new JPanel() {
+            @Override
+            //배경 이미지 그리기
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            }
+        };
+        panel.setPreferredSize(new Dimension(1000, 600));
+
+        add(panel);
+        pack();
+        setVisible(true);
+    }
+}
---
+++ SignUp/src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args){
+        Frame frame = new Frame();
+        frame.createFrame();
+    }
+
+
+}
